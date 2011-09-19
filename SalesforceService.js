e7d557fccfdf66b954470b451314d57b2eb88a44
tsschnoc <thomas.schnocklake@gmail.com>
2011-09-19 14:27:47 +0000

    commit2

--- SalesforceService.js
+++ SalesforceService.js
@@ -1,4 +1,4 @@
-SalesforceService = {
+Solve360Service = {
   _url : 'https://secure.solve360.com',
   _username : null,
   _password : null,
@@ -18,13 +18,59 @@ SalesforceService = {
   setUrl : function(url) {
     this._url = url;
   },
+  
+  salesforce_login : function(username, password, sfurl) {
+      var param = ["urn:login", ["urn:username", this._username],
+          ["urn:password", this._password]
+      ];
+      var result = doPartnerSoapRequest(
+        this._url + '/services/Soap/u/19.0',
+        param);
+      Logger.log(result.Envelope.Body.loginResponse.result.sessionId.getText());
+      Logger.log(result.Envelope.Body.loginResponse.result.serverUrl.getText());
+      //  return result.Envelope.Body.loginResponse.result.sessionId.getText();
+      var retParam = {};
+      retParam['sessionId'] = 
+        result.Envelope.Body.loginResponse.result.sessionId.getText();
+      retParam['serverUrl'] =
+        result.Envelope.Body.loginResponse.result.serverUrl.getText();
+      retParam['metadataServerUrl'] =
+        result.Envelope.Body.loginResponse.result.metadataServerUrl.getText();
+      return retParam;
+  },
 
-  helloWorls : function() {
-    return this._url;
+  doPartnerSoapRequest : function(url, body, header) {
+      var req = ["soapenv:Envelope",
+      {
+          "xmlns:soapenv": "http://schemas.xmlsoap.org/soap/envelope/"
+      }, {
+          "xmlns:meta": "http://soap.sforce.com/2006/04/metadata"
+      }, {
+          "xmlns:urn": "urn:partner.soap.sforce.com"
+      }, {
+          "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"
+      }, ["soapenv:Header", header],
+          ["soapenv:Body", body]
+      ];
+      Logger.log(body);
+      Logger.log(Xml.parseJS(req).toXmlString());
+      var options = {
+          "method": "post",
+          "contentType": "text/xml;charset=UTF-8",
+          "payload": Xml.parseJS(req).toXmlString(),
+          "headers": {
+              SOAPAction: "\"\""
+          }
+      };
+      var fetchRes = UrlFetchApp.fetch(url, options);
+      Logger.log(fetchRes.getContentText());
+      Logger.log(url);
+      var result = Xml.parse(fetchRes.getContentText(), false);
+      return result;
   },
 
 
 
 
-  dump : ''
+  dump = ''
 }
