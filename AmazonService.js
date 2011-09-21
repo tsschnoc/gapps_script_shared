40d7cc17e45963acfbf0b7c7494835d737817dc6
tsschnoc <thomas.schnocklake@gmail.com>
2011-09-21 11:36:04 +0000

    Amazon Signparam

--- AmazonService.js
+++ AmazonService.js
@@ -86,5 +86,71 @@ AmazonService = {
   },
   
   
+  signParams : function(HTTPVerb, ValueOfHostHeaderInLowercase,
+      HTTPRequestURI, queryStrings) {
+    var toSign = 
+      HTTPVerb + "\n" + 
+      ValueOfHostHeaderInLowercase + "\n" + 
+      HTTPRequestURI + "\n";
+    var timeStamp = 
+      Utilities.formatDate(new Date(), "GMT", "yyyy-MM-dd'T'HH:mm:ss.000'Z'");
+    
+    queryStrings.push({
+      key: "SignatureMethod",
+      value: "HmacSHA256"
+    });
+    queryStrings.push({
+      key: "SignatureVersion",
+      value: "2"
+    });
+    queryStrings.push({
+      key: "Timestamp",
+      value: encodeURIComponent(timeStamp)
+    });
+    queryStrings.push({
+      key: "AWSAccessKeyId",
+      value: encodeURIComponent(this._ACCESS_KEY_ID)
+    });
+    queryStrings.sort(function(a, b) {
+      if (a.key < b.key) {
+        return -1;
+      }
+      if (a.key < b.key) {
+        return 1;
+      }
+      return 0;
+    });
+    queryStrings.forEach(function(queryString, i) {
+      toSign += queryString.key + "=" + queryString.value + "&";
+    });
+    toSign = toSign.substr(0, toSign.length - 1);
+    Logger.log(toSign);
+    var step1 = Utilities.computeHmacSha256Signature(
+    toSign, this._SECRET_ACCESS_KEY);
+    var sig = Utilities.base64Encode(step1);
+    var signedParams = '?';
+    queryStrings.push({
+      key: "Signature",
+      value: encodeURIComponent(sig)
+    });
+    queryStrings.sort(function(a, b) {
+      if (a.key < b.key) {
+        return -1;
+      }
+      if (a.key < b.key) {
+        return 1;
+      }
+      return 0;
+    });
+    queryStrings.forEach(function(queryString, i) {
+      signedParams += queryString.key + "=" + queryString.value + "&";
+    });
+    signedParams = signedParams.substr(0, signedParams.length - 1);
+    Logger.log(signedParams);
+    return signedParams;
+  },
+  
+  
+  
   dump : ''
 };
