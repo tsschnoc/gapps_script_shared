8f19e814c469ff8bbe5b06dba7eaeec000533bce
tsschnoc <thomas.schnocklake@gmail.com>
2011-09-28 11:22:26 +0000

    getObjectField

--- SalesforceService.js
+++ SalesforceService.js
@@ -186,20 +186,16 @@ SalesforceService = {
   readObjectValueList: function(sf_objectname, fieldNames, where) {
     var lines = [];
     // Render result records into cells
-//    this.readObjectValues(sf_objectname, fieldNames, where).forEach(function(record, i) {
-    for (record in this.readObjectValues(sf_objectname, fieldNames, where)) {
+    this.readObjectValues(sf_objectname, fieldNames, where).forEach(function(record, i) {
       Logger.log('record' + record);
       var line = [];
-//      fieldNames.forEach(function(field, j) {
-      for (field in fieldNames) {
+      fieldNames.forEach(function(field, j) {
         //line.push(record[field]);
         line.push(SalesforceService.getValueInSobject(record, field));
-//      });
-      }
+      });
       //      Logger.log(line);
       lines.push(line);
-//    });
-    }
+    });
     return lines;
   },  
 
