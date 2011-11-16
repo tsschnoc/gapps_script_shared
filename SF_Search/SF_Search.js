c507e88c1889c33f846b89b5d4a4a683f4d94e75
tsschnoc <thomas.schnocklake@gmail.com>
2011-11-16 17:06:57 +0000

    sfsearch

--- SF_Search/SF_Search.js
+++ SF_Search/SF_Search.js
@@ -225,7 +225,7 @@ Ext.onReady(function() {
     var myObject = record.data.json;
     for (var i in myObject) {
       if (i != 'attributes' && i != 'type' && i != 'Id' && i != 'Name') {
-        if (myObject[i] != null) {}
+        if (myObject[i] != null) {
           if (myObject[i].substring(0, 1) == '+') {
             linetwo += i + ': ' + '<INPUT type=\"button\" value=\"' + myObject[i] + '\" onClick=\"javascript:callNumber(\'' + myObject[i].replace(/\s+/g, '') + '\')  \"><br/> ';
           }
