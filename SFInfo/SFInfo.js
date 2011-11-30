280522d0c9f6535fd97fdc6b9fe557ea0e9a76de
tsschnoc <thomas.schnocklake@gmail.com>
2011-11-30 16:39:05 +0000

    SFINFO

---
+++ SFInfo/SFInfo.js
@@ -0,0 +1,14 @@
+var matches = google.contentmatch.getContentMatches();
+var matchList = document.createElement('UL');
+var listItem;
+var extractedText;
+for (var match in matches) {
+  for (var key in matches[match]) {
+    listItem = document.createElement('LI');
+    extractedText = document.createTextNode(key + ": " + matches[match][key]);
+    listItem.appendChild(extractedText);
+    matchList.appendChild(listItem);
+  }
+}
+document.body.appendChild(matchList);
+gadgets.window.adjustHeight(100);
