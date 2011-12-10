872be88431ac43516c1f2418f5279724d3962ccb
tsschnoc <thomas.schnocklake@gmail.com>
2011-12-10 21:47:24 +0000

    SFTime

--- SFTime/calendar.js
+++ SFTime/calendar.js
@@ -323,6 +323,7 @@
     
     
     var ticket  = {};
+    ticket.Id = 'a03G0000005fhqDIAQ'
     ticket.Case__c = '500G0000008y77I';
     ticket.Description__c = 'vom gadget';
     ticket.Date__c = '2011-12-09';
