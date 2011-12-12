5e9de7e860f87edc4e46d2b8e65e1522e320fff2
tsschnoc <thomas.schnocklake@gmail.com>
2011-12-12 23:18:10 +0000

    SFTime

--- SFTime/calendar.js
+++ SFTime/calendar.js
@@ -43,7 +43,8 @@
     
     
 /////////////////////////////////////////////    
-    
+    var responseFunc;
+    var searchTerm;
    
     function log( message ) {
   		$( "<div/>" ).text( message ).prependTo( "#log" );
@@ -52,24 +53,9 @@
 
 		$( "#city" ).autocomplete({
 			source: function( request, response ) {
-				$.ajax({
-					url: "http://ws.geonames.org/searchJSON",
-					dataType: "jsonp",
-					data: {
-						featureClass: "P",
-						style: "full",
-						maxRows: 12,
-						name_startsWith: request.term
-					},
-					success: function( data ) {
-						response( $.map( data.geonames, function( item ) {
-							return {
-								label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
-								value: item.name
-							}
-						}));
-					}
-				});
+				responseFunc = response;
+        searchTerm = request;
+        
 			},
 			minLength: 2,
 			select: function( event, ui ) {
