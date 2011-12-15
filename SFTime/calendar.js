33f1b5100bfc78b1f224c5845bfc9c8b4d07c406
tsschnoc <thomas.schnocklake@gmail.com>
2011-12-15 07:21:27 +0000

    SFTime

--- SFTime/calendar.js
+++ SFTime/calendar.js
@@ -52,6 +52,13 @@
 		}
 
 		$( "#city" ).autocomplete({
+      max-height: 100px;
+  	  overflow-y: auto;
+		  /* prevent horizontal scrollbar */
+		  overflow-x: hidden;
+		  /* add padding to account for vertical scrollbar */
+		  padding-right: 20px;      
+      
 			source: function( request, response ) {
 				responseFunc = response;
         searchTerm = request;
