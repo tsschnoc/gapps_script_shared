9d5b8c74626e8528f22607e242a5b1d5fbd74d7d
tsschnoc <thomas.schnocklake@gmail.com>
2012-01-06 21:25:44 +0000

    SFTime

--- SFTime/calendar.js
+++ SFTime/calendar.js
@@ -288,9 +288,9 @@ var gcal_timecards = null;
       insEvent.description = JSON.stringify(sftimecard);
       insEvent.summary = sftimecard.Id;
       insEvent.start = {"dateTime": "2012-01-07T19:55:27.000Z"};
-      insEvent.start = {"dateTime": sftimecard.Date__c + "T" + sftimecard.TimeStart__c.substring(0, 2) + ":" + sftimecard.TimeStart__c.substring(2, 2) + ":00.000Z"};
+      insEvent.start = {"dateTime": sftimecard.Date__c + "T" + sftimecard.TimeStart__c.substring(0, 2) + ":" + sftimecard.TimeStart__c.substring(2, 4) + ":00.000Z"};
       insEvent.end = {"dateTime": "2012-01-07T20:55:27.000Z"};
-      insEvent.end = {"dateTime": sftimecard.Date__c + "T" + sftimecard.TimeStart__c.substring(0, 2) + ":" + sftimecard.TimeStart__c.substring(2, 2) + ":00.000Z"};
+      insEvent.end = {"dateTime": sftimecard.Date__c + "T" + sftimecard.TimeStart__c.substring(0, 2) + ":" + sftimecard.TimeStart__c.substring(2, 4) + ":00.000Z"};
       
       var postdata = JSON.stringify(insEvent);
       
