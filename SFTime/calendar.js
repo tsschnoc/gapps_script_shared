8a72c42cbed1f2bd282e0fc52106466fad5f0bac
tsschnoc <thomas.schnocklake@gmail.com>
2012-01-27 20:47:14 +0000

    SFTime

--- SFTime/calendar.js
+++ SFTime/calendar.js
@@ -87,7 +87,7 @@ function receiver(event) {
       oAuthToken = response.data;
       var params = {};
     	params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
-  		params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.POST;
+  		params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.GET;
   		params[gadgets.io.RequestParameters.POST_DATA] = postdata;
     	params[gadgets.io.RequestParameters.HEADERS] = {
   			"Accept": "application/json",
