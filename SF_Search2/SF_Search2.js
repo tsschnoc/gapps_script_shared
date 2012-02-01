d58cad1898568e9eafa3b4e1ffd26fda0424fbcc
tsschnoc <thomas.schnocklake@gmail.com>
2012-02-01 21:43:10 +0000

    SF_Search2

--- SF_Search2/SF_Search2.js
+++ SF_Search2/SF_Search2.js
@@ -12,60 +12,8 @@ function makeCachedRequest(url, callback, params, refreshInterval) {
 }
 
 
-// Display UI depending on OAuth access state of the gadget (see <divs> above).
-// If user hasn't approved access to data, provide a "Personalize this gadget" link
-// that contains the oauthApprovalUrl returned from makeRequest.
-//
-// If the user has opened the popup window but hasn't yet approved access, display
-// text prompting the user to confirm that s/he approved access to data.  The user
-// may not ever need to click this link, if the gadget is able to automatically
-// detect when the user has approved access, but showing the link gives users
-// an option to fetch their data even if the automatic detection fails.
-//
-// When the user confirms access, the fetchData() function is invoked again to
-// obtain and display the user's data.
 
 
-function showOneSection(toshow) {
-  var sections = ['main', 'approval', 'waiting'];
-  for (var i = 0; i < sections.length; ++i) {
-    var s = sections[i];
-    var el = document.getElementById(s);
-    if (s === toshow) {
-      el.style.display = "block";
-    }
-    else {
-      el.style.display = "none";
-    }
-  }
-}
-
-// Process returned JSON feed to display data.
-
-
-function showResults(result) {
-  showOneSection('main');
-
-  var titleElement = document.createElement('div');
-  var nameNode = document.createTextNode(result.feed.title.$t);
-  titleElement.appendChild(nameNode);
-  document.getElementById("main").appendChild(titleElement);
-  document.getElementById("main").appendChild(document.createElement("br"));
-
-  list = result.feed.entry;
-
-  for (var i = 0; i < list.length; i++) {
-    entry = list[i];
-    var divElement = document.createElement('div');
-    divElement.setAttribute('class', 'name');
-    divElement.appendChild(nameNode);
-    if (entry.gd$email) {
-      var valueNode = document.createTextNode(entry.gd$email[0].address);
-      divElement.appendChild(valueNode);
-    }
-    document.getElementById("main").appendChild(divElement);
-  }
-}
 
 // Invoke makeRequest() to fetch data from the service provider endpoint.
 // Depending on the results of makeRequest, decide which version of the UI
@@ -109,11 +57,12 @@ function fetchData() {
       personalize.onclick = popup.createOpenerOnClick();
       var approvaldone = document.getElementById('approvaldone');
       approvaldone.onclick = popup.createApprovedOnClick();
-      showOneSection('approval');
+//      showOneSection('approval');
     }
     else if (response.data) {
-      showOneSection('main');
-      showResults(response.data);
+      console.log(response.data);
+//      showOneSection('main');
+//      showResults(response.data);
     }
     else {
       // The response.oauthError and response.oauthErrorText values may help debug
@@ -121,9 +70,19 @@ function fetchData() {
       var main = document.getElementById('main');
       var err = document.createTextNode('OAuth error: ' + response.oauthError + ': ' + response.oauthErrorText);
       main.appendChild(err);
-      showOneSection('main');
+//      showOneSection('main');
     }
   }, params);
 }
+
+function initGadget() {
+  google.load("jquery", "1.4.2");
+  google.load("jqueryui", "1.7.2");
+
+  google.setOnLoadCallback(function() {
+    fetchData();
+  });
+}
+
 // Call fetchData() when gadget loads.
-gadgets.util.registerOnLoadHandler(fetchData);
+gadgets.util.registerOnLoadHandler(initGadget);
