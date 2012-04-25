76dda5cf52f33dd684c92ea7114c93d2d73b04f2
ide user ide_tsschnoc <thomas.schnocklake@gmail.com>
2012-04-25 13:21:51 +0000

    pusher

--- pusher/pushergadget.js
+++ pusher/pushergadget.js
@@ -47,13 +47,13 @@ window.WebSocket,Pusher.TransportType="flash",window.WEB_SOCKET_SWF_LOCATION=a+"
 
 
 
-        function popitup(url) {
-            newwindow=window.open(url,'name','height=600,width=800');
-            if (window.focus) {
-                newwindow.focus()
-            }
-            return false;
+    function popitup(url) {
+        newwindow=window.open(url,'name','height=600,width=800');
+        if (window.focus) {
+            newwindow.focus()
         }
+        return false;
+    }
 
 
 
@@ -76,131 +76,74 @@ window.WebSocket,Pusher.TransportType="flash",window.WEB_SOCKET_SWF_LOCATION=a+"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Display UI depending on OAuth access state of the gadget (see <divs> above).
-    // If user hasn't approved access to data, provide a "Personalize this gadget" link
-    // that contains the oauthApprovalUrl returned from makeRequest.
-    //
-    // If the user has opened the popup window but hasn't yet approved access, display
-    // text prompting the user to confirm that s/he approved access to data.  The user
-    // may not ever need to click this link, if the gadget is able to automatically
-    // detect when the user has approved access, but showing the link gives users
-    // an option to fetch their data even if the automatic detection fails.
-    //
-    // When the user confirms access, the fetchData() function is invoked again to
-    // obtain and display the user's data.
-    function showOneSection(toshow) {
-      var sections = [ 'main', 'approval', 'waiting' ];
-      for (var i=0; i < sections.length; ++i) {
-        var s = sections[i];
-        var el = document.getElementById(s);
-        if (s === toshow) {
-          el.style.display = "block";
-        } else {
-          el.style.display = "none";
-        }
-      }
+  function makeCachedRequest(url, callback, params, refreshInterval) {
+    var ts = new Date().getTime();
+    var sep = "?";
+    if (refreshInterval && refreshInterval > 0) {
+      ts = Math.floor(ts / (refreshInterval * 1000));
     }
-
-    // Process returned JSON feed to display data.
-    function showResults(result) {
-      showOneSection('main');
-
-      var titleElement = document.createElement('div');
-      var nameNode = document.createTextNode(result.feed.title.$t);
-      titleElement.appendChild(nameNode);
-      document.getElementById("main").appendChild(titleElement);
-      document.getElementById("main").appendChild(document.createElement("br"));
-
-      list = result.feed.entry;
-
-      for(var i = 0; i < list.length; i++) {
-        entry = list[i];
-        var divElement = document.createElement('div');
-        divElement.setAttribute('class', 'name');
-        divElement.appendChild(nameNode);
-        if (entry.gd$email) {
-          var valueNode = document.createTextNode(entry.gd$email[0].address);
-          divElement.appendChild(valueNode);
-        }
-        document.getElementById("main").appendChild(divElement);
-      }
+    if (url.indexOf("?") > -1) {
+      sep = "&";
     }
+    url = [url, sep, "nocache=", ts].join("");
+    gadgets.io.makeRequest(url, callback, params);
+  }
 
-    // Invoke makeRequest() to fetch data from the service provider endpoint.
-    // Depending on the results of makeRequest, decide which version of the UI
-    // to ask showOneSection() to display. If user has approved access to his
-    // or her data, display data.
-    // If the user hasn't approved access yet, response.oauthApprovalUrl contains a
-    // URL that includes a Google-supplied request token. This is presented in the
-    // gadget as a link that the user clicks to begin the approval process.
-    function fetchData() {
+
+
+  function popupMessageReceiver(event) {
+    //this function is called by the popup when it opens the oauth-callback-page and passed the loaded url back
+    
+    alert ('Message received: ' + event.origin + ' : '  + event.data);
+    
+    if (SF_RequestToken === null) SF_RequestToken = {};
+
+    if (event.origin == 'https://s3.amazonaws.com') {
+      var pairs = event.data.split('?')[1].split('&');
+      for (var i in pairs) {
+        var kv = pairs[i].split('=');
+        SF_RequestToken[kv[0]] = decodeURIComponent(kv[1]);
+      }
+
+      debug(SF_RequestToken);
+
+      var postdata = 'grant_type=authorization_code&' + 'code=' + encodeURIComponent(SF_RequestToken.code) + '&client_id=' + encodeURIComponent(consumerKey) + '&client_secret=' + encodeURIComponent(consumerSecret) + '&redirect_uri=' + encodeURIComponent(oauth2_callbackurl) + '&state=gettoken&format=json';
+
       var params = {};
-      url = "http://www.google.com/m8/feeds/contacts/default/base?alt=json";
       params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
-      params[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.OAUTH;
-      params[gadgets.io.RequestParameters.OAUTH_SERVICE_NAME] = "google";
-      params[gadgets.io.RequestParameters.OAUTH_USE_TOKEN] = "always";
-      params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.GET;
+      params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.POST;
+      params[gadgets.io.RequestParameters.POST_DATA] = postdata;
+      params[gadgets.io.RequestParameters.HEADERS] = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-PrettyPrint": "1"
+      };
 
-      gadgets.io.makeRequest(url, function (response) {
-        if (response.oauthApprovalUrl) {
-          // Create the popup handler. The onOpen function is called when the user
-          // opens the popup window. The onClose function is called when the popup
-          // window is closed.
-          var popup = shindig.oauth.popup({
-            destination: response.oauthApprovalUrl,
-            windowOptions: null,
-            onOpen: function() { showOneSection('waiting'); },
-            onClose: function() { fetchData(); }
-          });
-          // Use the popup handler to attach onclick handlers to UI elements.  The
-          // createOpenerOnClick() function returns an onclick handler to open the
-          // popup window.  The createApprovedOnClick function returns an onclick
-          // handler that will close the popup window and attempt to fetch the user's
-          // data again.
-          var personalize = document.getElementById('personalize');
-          personalize.onclick = popup.createOpenerOnClick();
-          var approvaldone = document.getElementById('approvaldone');
-          approvaldone.onclick = popup.createApprovedOnClick();
-          showOneSection('approval');
-        } else if (response.data) {
-            showOneSection('main');
-            showResults(response.data);
-        } else {
-            // The response.oauthError and response.oauthErrorText values may help debug
-            // problems with your gadget.
-            var main = document.getElementById('main');
-            var err = document.createTextNode('OAuth error: ' +
-              response.oauthError + ': ' + response.oauthErrorText);
-            main.appendChild(err);
-            showOneSection('main');
-        }
-      }, params);
+      makeCachedRequest('https://login.salesforce.com/services/oauth2/token', oauth2_callback, params);
     }
-    // Call fetchData() when gadget loads.
-    gadgets.util.registerOnLoadHandler(fetchData);
+  }
+
+
+
+
+
+    function gadgetOnLoad() {
+      window.addEventListener('message', popupMessageReceiver, false);    
+
+      var scope = 'http://www.google.com/m8/feeds/';
+      var oauth2_callbackurl = 'https://s3.amazonaws.com/tsschnocwinn/oAuthcallback.html';
+      var client_id = '759881060264-k2s770vd2ghjbo2d90fq972kqoo9b0ma.apps.googleusercontent.com';
+    
+      var authLink = 'https://accounts.google.com/o/oauth2/auth?scope=' + encodeURIComponent(scope) + '&state=state1&redirect_uri=' + encodeURIComponent(oauth2_callbackurl) + '&response_type=code&client_id=' + encodeURIComponent(client_id) + '&approval_prompt=force';
+
+popitup(authLink) ;
+    }
+
+
+
+
+
+
+    gadgets.util.registerOnLoadHandler(gadgetOnLoad);
 
 
 
