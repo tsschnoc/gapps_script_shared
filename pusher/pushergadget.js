0e443d829fd4eef56eeda2ecc504837d2516a44d
ide user ide_tsschnoc <thomas.schnocklake@gmail.com>
2012-04-25 11:45:30 +0000

    pusher

--- pusher/pushergadget.js
+++ pusher/pushergadget.js
@@ -67,9 +67,9 @@ window.WebSocket,Pusher.TransportType="flash",window.WEB_SOCKET_SWF_LOCATION=a+"
 
     var pusher = new Pusher('0bcfb89cee9d117b2b4e');
     var channel = pusher.subscribe('test_channel');
-    channel.bind('my_event', function(data) {
+    channel.bind('my_event', function(phoneCall) {
 //      popitup("http://www.schnocklake.de?number" + data);
-      var x = '<a href="http://www.google.com">' + data.key + '</a>'; 
+      var x = '<a href="http://www.google.com">' + phoneCall.number + '</a>'; 
       $("#ny").html(x);
     });
 
