ecbab7b2561ac5c994151390cbbb4463af58268e
ide user ide_tsschnoc <thomas.schnocklake@gmail.com>
2012-05-27 12:12:39 +0000

    SF

--- pusher/pushergadget.js
+++ pusher/pushergadget.js
@@ -99,7 +99,8 @@ function receiveCall(phoneCall) {
 var ResultItem = Backbone.Model.extend({});
 var ResultItemListView = Backbone.View.extend({
   tagName: 'a',
-  template: _.template('<img border="0" src="http://www.salesforce.com/favicon.ico" alt="google contact" width="10" height="10"><%= label %><div style="font-size: 70%; "><%= label %></div></a>'),
+//  template: _.template('<img border="0" src="http://www.salesforce.com/favicon.ico" alt="google contact" width="10" height="10"><%= label %><div style="font-size: 70%; "><%= label %></div>'),
+  template: _.template($('#itemDetail-template').html()),
 
   render: function(){
 //    $(this.el).html('<div>' + this.model.get('label') + '</div>');
